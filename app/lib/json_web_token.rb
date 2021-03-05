e64db1f5c551b5bf351ca28ef26d82e9c16dd3c4
rindra josia <rrindrajosia@gmail.com>
2021-03-05 03:55:12 +0300

    Add secrets.yml file

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -3,11 +3,11 @@ class JsonWebToken
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, HMAC_SECRET)
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
   end
 
   def self.decode(token)
-    body = JWT.decode(token, HMAC_SECRET)[0]
+    body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
     HashWithIndifferentAccess.new body
   rescue JWT::DecodeError => e
     raise ExceptionHandler::InvalidToken, e.message
