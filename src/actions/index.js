f23b9b946f468c4d08dbad6547acd0e0a412f3ec
rindra josia <rrindrajosia@gmail.com>
2021-03-06 13:42:17 +0300

    Fetch products, favorites

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,6 +11,12 @@ import {
   FETCH_PRODUCTS_REQUEST,
   FETCH_PRODUCTS_SUCCESS,
   FETCH_PRODUCTS_FAILURE,
+  FETCH_FAVORITES_REQUEST,
+  FETCH_FAVORITES_SUCCESS,
+  FETCH_FAVORITES_FAILURE,
+  FETCH_FAVORITE_PRODUCTS_REQUEST,
+  FETCH_FAVORITE_PRODUCTS_SUCCESS,
+  FETCH_FAVORITE_PRODUCTS_FAILURE,
 } from './actionTypes';
 
 export const createUserRequest = () => ({
@@ -69,6 +75,34 @@ export const fetchProductsFailure = error => ({
   payload: error,
 });
 
+export const fetchFavoritesRequest = () => ({
+  type: FETCH_FAVORITES_REQUEST,
+});
+
+export const fetchFavoritesSuccess = favorites => ({
+  type: FETCH_FAVORITES_SUCCESS,
+  payload: data,
+});
+
+export const fetchFavoritesFailure = error => ({
+  type: FETCH_FAVORITES_FAILURE,
+  payload: error,
+});
+
+export const fetchFavoriteProductsRequest = () => ({
+  type: FETCH_FAVORITE_PRODUCTS_REQUEST,
+});
+
+export const fetchFavoriteProductsSuccess = favorites => ({
+  type: FETCH_FAVORITE_PRODUCTS_SUCCESS,
+  payload: data,
+});
+
+export const fetchFavoriteProductsFailure = error => ({
+  type: FETCH_FAVORITE_PRODUCTS_FAILURE,
+  payload: error,
+});
+
 export const createUser = (url, data) => dispatch => {
   dispatch(createUserRequest);
   return fetch(url,{
@@ -116,4 +150,77 @@ export const fetchCategories = url => dispatch => {
     .catch(error => {
       dispatch(fetchCategoriesFailure(error));
     });
+};
+
+export const fetchProducts = url => dispatch => {
+  dispatch(fetchProductsRequest);
+  return fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data),
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error - 404 not found');
+    }
+    return response.json();
+  })
+  .then(data => {
+    dispatch(fetchProductsSuccess(data));
+  })
+  .catch(error => {
+    dispatch(fetchProductsFailure(error));
+  });
+};
+
+export const fetchFavorites = (url, token, role) => dispatch => {
+  dispatch(fetchFavoritesRequest);
+  return fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `${token}`,
+      'Accept': `application/vnd.shop.${role}+json`
+    },
+    body: JSON.stringify(data),
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error - 404 not found');
+    }
+    return response.json();
+  })
+  .then(data => {
+    dispatch(fetchFavoritesSuccess(data));
+  })
+  .catch(error => {
+    dispatch(fetchFavoritesFailure(error));
+  });
+};
+
+export const fetchFavoriteProducts = (url, token, role) => dispatch => {
+  dispatch(fetchFavoriteProductsRequest);
+  return fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `${token}`,
+      'Accept': `application/vnd.shop.${role}+json`
+    },
+    body: JSON.stringify(data),
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Error - 404 not found');
+    }
+    return response.json();
+  })
+  .then(data => {
+    dispatch(fetchFavoriteProductsSuccess(data));
+  })
+  .catch(error => {
+    dispatch(fetchFavoriteProductsFailure(error));
+  });
 };
